2650e4aaf3f29fa88adbe897bed3e2f3d2fcf339
Prasanth Gururaj <prasanth4254@gmail.com>
2025-10-18 23:14:14 -0700

    Initital commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'youtube',
+    version= '0.0.0',
+    author= 'Prasanth',
+    author_email= 'prasanth4254@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
